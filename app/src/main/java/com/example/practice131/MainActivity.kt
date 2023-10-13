4de9bc3d9db8e0c24551f557555206415095f2ee
Viktoriiafedorcenko <vfedorcenko568@gmail.com>
2023-10-13 21:18:34 +0300

    first commit

--- app/src/main/java/com/example/practice131/MainActivity.kt
+++ app/src/main/java/com/example/practice131/MainActivity.kt
@@ -9,6 +9,7 @@ import kotlin.math.sqrt
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 
@@ -20,19 +21,33 @@ class MainActivity: AppCompatActivity() {
 
 
         val editTextNumber = findViewById<EditText>(R.id.editTextNumber)
-        val editTextNumber2 = findViewById<EditText>(R.id.editTextNumber2)
-        val textView=findViewById<TextView>(R.id.textView)
-        val button = findViewById<Button>(R.id.button)
-        button.setOnClickListener{
-            val editTextNumber=editTextNumber.text.toString().toDouble()
-            val editTextNumber2=editTextNumber2.text.toString().toDouble()
-            val hupotenuse=sqrt(editTextNumber * editTextNumber2) + (editTextNumber2 * editTextNumber2)
-            val perimetr=editTextNumber+editTextNumber2*hupotenuse
-            val area=0.5*editTextNumber*editTextNumber2
-            val textView = "Гипотенуза треугольника: $hupotenuse\n" +
-                    "Периметр треугольника: $perimetr\n" +"Площадь треугольника: $area"
+        editTextNumber.requestFocus()
+    }
 
+    fun buttonOnClick(view: View) {
+        val editTextNumber = findViewById<EditText>(R.id.editTextNumber)
+        val editTextNumber2 = findViewById<EditText>(R.id.editTextNumber2)
+        val textView = findViewById<TextView>(R.id.textView)
+        val textView2 = findViewById<TextView>(R.id.textView2)
+        val textView3 = findViewById<TextView>(R.id.textView3)
+        if (editTextNumber.text.toString() == "") {
+            editTextNumber.requestFocus()
+        } else if (editTextNumber2.text.toString() == "") {
+            Toast.makeText(this, "Введите 1-ый катет ! ", Toast.LENGTH_SHORT).show()
+            editTextNumber2.requestFocus()
+        } else if (editTextNumber2.text.toString() == "") {
+            Toast.makeText(this, "Введите 2-ый катет !", Toast.LENGTH_SHORT).show()
+            editTextNumber2.requestFocus()
+        } else {
+            val cathetus1: Double = editTextNumber!!.text.toString().toDouble()
+            val cathetus2: Double = editTextNumber2!!.text.toString().toDouble()
+            textView.setText((cathetus1 * cathetus2 * 0.5).toString())
+            textView2.setText((cathetus1 * cathetus1 + Math.sqrt(cathetus1 * cathetus1 + cathetus2 * cathetus2)).toString())
+            textView3.setText(
+                (Math.sqrt((cathetus1 * cathetus1 + cathetus2 * cathetus2)).toString())
+            )
 
         }
+
     }
 }
